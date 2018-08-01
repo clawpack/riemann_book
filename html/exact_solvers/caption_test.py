f5b9364fc8356962ebdf787bcd29e51fcd833824
Randy LeVeque <rjl@uw.edu>
2018-08-01 13:33:12 -0700

    add html/exact_solvers that are linked from notebooks

---
+++ html/exact_solvers/caption_test.py
@@ -0,0 +1,5 @@
+
+def caption1():
+    from latex_envs.latex_envs import figcaption 
+    figcaption("Riemann solutions to the LWR model with a convex flux function.", 
+           label="fig:nonconvex-LWR")
---
+++ html/exact_solvers/test.py
@@ -0,0 +1,7 @@
+
+import euler
+print(euler)
+print('ok')
+
+def ff(x):
+    print(x+1)
