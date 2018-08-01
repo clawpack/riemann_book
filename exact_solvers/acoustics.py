e715668e72d2cae31e691c578ac4e689057ffa09
maojrs <maojrs@gmail.com>
2018-08-01 12:04:35 +0200

    Added full interactive solution w/phase plane

--- exact_solvers/acoustics.py
+++ exact_solvers/acoustics.py
@@ -1,7 +1,6 @@
 """
-Exact Riemann solvers for the acoustic wave equation in 1D.
-Includes separate solvers for homogeneous and heterogeneous
-media.
+Exact Riemann solver for the acoustics wave equation in 1D, 
+including some plotting functionality.
 """
 import numpy as np
 import sys
@@ -9,17 +8,18 @@ sys.path.append('../utils')
 from utils import riemann_tools
 
 def lambda1(q, xi, aux):
-    "Characteristic speed for 1-waves."
+    """Characteristic speed for 1-waves."""
     rho, bulk = aux
     return -np.sqrt(bulk/rho)
 
 def lambda2(q, xi, aux):
-    "Characteristic speed for 2-waves."
+    """Characteristic speed for 2-waves."""
     rho, bulk = aux
     return np.sqrt(bulk/rho)
 
 def exact_riemann_solution(ql, qr, aux):
-
+    """ Exact solution of Riemann problem for acoustics  equations."""
+    
     # Define delta q, speeds and impedance
     rho, bulk = aux
     dq = qr - ql
@@ -62,8 +62,8 @@ def exact_riemann_solution(ql, qr, aux):
     return states, speeds, reval
 
 
-# Return Riemann shock function for interacts
-def riemann_plot(q_l, q_r, aux):
+def riemann_plot_func(q_l, q_r, aux):
+    """Return Riemann plot function for (only) time-dependent interacts. """
     ex_states, ex_speeds, reval = exact_riemann_solution(q_l ,q_r, aux)
 
     plot_function = riemann_tools.make_plot_function(ex_states, ex_speeds, reval, layout='vertical',
@@ -72,61 +72,6 @@ def riemann_plot(q_l, q_r, aux):
                                                     plot_chars=[lambda1, lambda2])
     return plot_function
 
-# Functions for heterogeneous media
 
-def lambda_1_het(q, xi, aux):
-    "Characteristic speed for 1-waves in a heterogeneous medium."
-    rho, K = aux
-    return -np.sqrt(K/rho)
 
-def lambda_2_het(q, xi, aux):
-    "Characteristic speed for 2-waves in a heterogeneous medium."
-    rho, K = aux
-    return np.sqrt(K/rho)
 
-def exact_riemann_heterogenous(ql, qr, auxl, auxr):
-
-    # Define delta q, speeds and impedance (left and right)
-    dq = qr - ql
-    rhol, bulkl = auxl
-    rhor, bulkr = auxr
-    cl = np.sqrt(bulkl/rhol)
-    cr = np.sqrt(bulkr/rhor)
-    Zl = rhol*cl
-    Zr = rhor*cr
-
-    # Define the 2 eigenvectors
-    r1 = np.array([-Zl, 1])
-    r2 = np.array([Zr,  1])
-
-    # Compute the alphas
-    alpha1 = (-dq[0] + dq[1]*Zr)/(Zl + Zr)
-    alpha2 = (dq[0] + dq[1]*Zl)/(Zl + Zr)
-
-    # Compute middle state qm
-    qm = ql + alpha1*r1
-    # It is equivalent to
-    #qm = qr - alpha2*r2
-
-    # Compute waves speeds (characteristics)
-    speeds = np.zeros(2)
-    speeds[0] = -cl
-    speeds[1] = cr
-
-    # Concatenate states for plotting
-    states = np.column_stack([ql,qm,qr])
-
-    # Calculate reval function (only necessary for plotting)
-    def reval(xi):
-            r"""Returns the Riemann solution in for any value of xi = x/t.
-            """
-            p_out =  (xi<=speeds[0]                 )*ql[0]      \
-                    + (xi>speeds[0])*(xi<=speeds[1])*qm[0]    \
-                    + (xi>speeds[1]                 )*qr[0]
-
-            u_out =  (xi<=speeds[0]                 )*ql[1]      \
-                    + (xi>speeds[0])*(xi<=speeds[1])*qm[1]    \
-                    + (xi>speeds[1]                 )*qr[1]
-            return p_out, u_out
-
-    return states, speeds, reval
--- exact_solvers/acoustics_demos.py
+++ exact_solvers/acoustics_demos.py
@@ -1,12 +1,19 @@
+"""
+Additional functions and demos for acoustics equations.
+"""
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import numpy as np
 from ipywidgets import widgets, interact
 from IPython.display import display
+import sys
+sys.path.append('../utils')
+from utils import riemann_tools
 from . import acoustics
 colors = ['g','orange']
 
-# Decomposed eigenvectors interactive
 def decompose_q_interactive():
+    """plots interactive decomposed eigenvectors."""
     pwidget = widgets.FloatSlider(min=-1,max=1,value=1.)
     uwidget = widgets.FloatSlider(min=-1,max=1,value=0.)
     rhowidget = widgets.FloatSlider(min=0.1,max=2,value=1.,description=r'$\rho$')
@@ -22,7 +29,8 @@ def decompose_q_interactive():
     display(mainwidget.widget.out)
 
 def decompose_q(p,u,K,rho):
-    # Should also print the eigenvectors and the values w_1, w_2
+    r"""Plotting function for decompose_q_interactive. It 
+    should also print the eigenvectors and the values w_1, w_2."""
     Z = np.sqrt(K*rho)
     fig, axes = plt.subplots(1,2,figsize=(10,6))
     axes[0].arrow(0,0,-Z,1,head_width=0.05, head_length=0.1, color=colors[0])
@@ -52,8 +60,9 @@ def decompose_q(p,u,K,rho):
 
     plt.tight_layout()
 
-# Characteristics solution interactive
+
 def char_solution_interactive():
+    """Plots interactive characteristics solution."""
     twidget = widgets.FloatSlider(min=0.,max=2,value=0.)
     rhowidget = widgets.FloatSlider(min=0.1,max=2,value=1.,description=r'$\rho$')
     Kwidget = widgets.FloatSlider(min=0.1,max=2,value=1.)
@@ -67,6 +76,7 @@ def char_solution_interactive():
     display(mainwidget.widget.out)
 
 def char_solution(t, K, rho):
+    """Plotting function for char_solution_interactive."""
     fig, axes = plt.subplots(1,2,figsize=(11.5,5.5))
     c = np.sqrt(K/rho)
     x = np.linspace(-2*c-1,2*c+1,41)
@@ -94,9 +104,10 @@ def char_solution(t, K, rho):
     axes[1].set_xlabel('$x$')
     axes[1].set_ylabel('$t$')
 
-# Phase plane plot for acoustic equations.
+
 def phase_plane_plot():
-    "Return phase plane function ready to use with interact."
+    """Plots phase plane, also used by interactive_phase_plane
+    since it returns phase plane function ready to use with interact."""
 
     def plot_function(pl,ul,pr,ur,rho,bulk,
                       xmin=0,xmax=6,ymin=-6,ymax=6,show_phys=True,show_unphys=True):
@@ -155,11 +166,10 @@ def phase_plane_plot():
         plt.show()
     return plot_function
 
-# Interactive phase plane plot for acoustic equations.
-def interactive_phase_plane(ql=(10.0, -5.0),
-                                      qr=(40.0, 5.0),
-                                      rho=2.0, bulk=1.0):
-    "Create the GUI and output the interact app."
+
+def interactive_phase_plane(ql=(10.0, -5.0), qr=(40.0, 5.0), rho=2.0, bulk=1.0):
+    """Plots interactive phase plane plot."""
+
     # Create plot function for interact
     pp_plot = phase_plane_plot()
 
@@ -217,5 +227,165 @@ def interactive_phase_plane(ql=(10.0, -5.0),
     ppwidget.widget.close()
     display(interact_gui)
     display(ppwidget.widget.out)
-   
 
+
+def full_riemann_solution_plot():
+    """Plots full Riemann solution, including the phase plane, also used by 
+    full_riemann_interactive since it returns plot function ready to use with interact."""
+    
+    def plot_function(t,pl,ul,pr,ur,rho,bulk,which_char,
+                      xmin=0,xmax=6,ymin=-6,ymax=6,show_phys=True,show_unphys=True):
+        "Subfunction required for interactive (function of only interactive parameters)."
+    
+        # Define parameters
+        dp = pr - pl
+        du = ur - ul
+        c = np.sqrt(bulk/rho)
+        Z = rho*c
+        
+        # Define eigenvectors and functions
+        eig1 = np.array([-Z, 1])
+        eig2 = np.array([Z, 1])
+        lin1l = lambda p: ul - 1./Z*(p-pl) 
+        lin2l = lambda p: ul + 1./Z*(p-pl) 
+        lin1r = lambda p: ur - 1./Z*(p-pr) 
+        lin2r = lambda p: ur + 1./Z*(p-pr) 
+        
+        
+        # Solve Riemann problem       
+        aux = [rho,bulk]
+        states, speeds, riemann_eval = acoustics.exact_riemann_solution(np.array([pl,ul]), np.array([pr,ur]), aux)
+        pm = states[0][1]
+        um = states[1][1]
+
+        
+        # Set figure grid
+        fig = plt.figure(figsize=(11.5, 5.5))
+        outer_grid = gridspec.GridSpec(1, 2, wspace=0.15, hspace=0.15)
+        inner_grid = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=outer_grid[0], wspace=0.0, hspace=0.0)
+        ax1 = plt.Subplot(fig, inner_grid[0]) # x-t plane
+        ax2 = plt.Subplot(fig, inner_grid[1]) # x vs pressure
+        ax3 = plt.Subplot(fig, inner_grid[2]) # x vs velocity
+        ax4 = plt.Subplot(fig, outer_grid[1]) # phase plane
+        ax1.set_ylabel("t", fontsize=10)
+        ax2.set_ylabel("pressure", fontsize=10)
+        ax3.set_ylabel("velocity", fontsize=10)
+        ax3.set_xlabel("x", fontsize=10)
+        ax1.set_xticks([])
+        ax2.set_xticks([])
+
+        # Plot Riemann solution on ax1, ax2 and ax3
+        ax = np.array([ax1, ax2, ax3])
+        riemann_tools.plot_riemann(states, speeds, riemann_eval, wave_types=None, t=t, ax=ax, 
+                                   layout='vertical', variable_names=['pressure', 'velocity'])
+        
+        # Plot characteristics on ax1 if required
+        if which_char:
+            plot_chars=[acoustics.lambda1, acoustics.lambda2]
+            riemann_tools.plot_characteristics(riemann_eval, plot_chars[which_char-1],
+                                     aux=(np.array(aux),np.array(aux)), axes=ax[0], speeds=speeds)
+        
+        # Plot solution in phase plane plot ion ax4
+        x = (pl, pr, pm)
+        y = (ul, ur, um)
+        dx, dy = xmax - xmin, ymax - ymin
+        ax4.set_xlim(min(0.00000001,xmin),xmax)
+        ax4.set_ylim(ymin,ymax)
+        ax4.set_xlabel('Pressure (p)', fontsize=10)
+        ax4.set_ylabel('Velocity (u)', fontsize=10)
+        ax4.set_title('Phase plane', fontsize=12)
+        p = np.linspace(xmin,xmax,500)
+
+        # Plot unphysical solutions
+        if show_unphys:
+            ax4.plot(p,lin2l(p),'--k')
+            ax4.plot(p,lin1r(p),'--k')
+
+        # Plot physical solutions
+        if show_phys:
+            ax4.plot(p,lin1l(p),'-k')
+            ax4.plot(p,lin2r(p),'-k')
+            if (pm>=0 and pm <= xmax and um > ymin and um < ymax):
+                ax4.plot(pm, um, '-ok', markersize=10)
+                ax4.text(x[2] + 0.03*dx,y[2] + 0.03*dy, '$q_m$', fontsize=15)
+
+        # Plot initial states and markers
+        ax4.plot(pl, ul, '-ok', markersize=10)
+        ax4.plot(pr, ur, '-ok', markersize=10)
+        for i,label in enumerate(('$q_l$', '$q_r$')):
+            ax4.text(x[i] + 0.03*dx,y[i] + 0.03*dy,label, fontsize=15)
+            
+        # Add all plots to fig and show    
+        fig.add_subplot(ax1)
+        fig.add_subplot(ax2)
+        fig.add_subplot(ax3)
+        fig.add_subplot(ax4)
+        plt.show()
+    return plot_function
+
+
+def full_riemann_interactive(ql=(10.0, -5.0), qr=(40.0, 5.0), rho=2.0, bulk=1.0):
+    """Plots interactive full riemann solution with phase plane plot."""
+
+    # Create plot function for interact
+    pp_plot = full_riemann_solution_plot()
+
+    # Declare all widget sliders
+    t_widget = widgets.FloatSlider(value=0,min=0.0,max=1.0, description='$t$')
+    ql1_widget = widgets.FloatSlider(value=ql[0],min=0.01,max=50.0, description='$p_l$')
+    ql2_widget = widgets.FloatSlider(value=ql[1],min=-30,max=30.0, description='$u_l$')
+    qr1_widget = widgets.FloatSlider(value=qr[0],min=0.01,max=50.0, description='$p_r$')
+    qr2_widget = widgets.FloatSlider(value=qr[1],min=-30,max=30.0, description='$u_r$')
+    rho_widget = widgets.FloatSlider(value=rho,min=0.01,max=10.0, description=r'$\rho$')
+    bulk_widget = widgets.FloatSlider(value=bulk,min=0.01,max=10.0, description='$K$')
+    xmin_widget = widgets.BoundedFloatText(value=0.0000001, description='$p_{min}:$')
+    xmax_widget = widgets.FloatText(value=50, description='$p_{max}:$')
+    ymin_widget = widgets.FloatText(value=-30, description='$u_{min}:$')
+    ymax_widget = widgets.FloatText(value=30, description='$u_{max}:$')
+    which_char_widget = widgets.Dropdown(options=[None,1,2],description='Characs.')
+    show_physical = widgets.Checkbox(value=True, description='Physical solution')
+    show_unphysical = widgets.Checkbox(value=True, description='Unphysical solution')
+
+    # Allow for dependent widgets to update
+    def update_xmin(*args):
+        ql1_widget.min = xmin_widget.value
+        qr1_widget.min = xmin_widget.value
+    def update_xmax(*args):
+        ql1_widget.max = xmax_widget.value
+        qr1_widget.max = xmax_widget.value
+    def update_ymin(*args):
+        ql2_widget.min = ymin_widget.value
+        qr2_widget.min = ymin_widget.value
+    def update_ymax(*args):
+        ql2_widget.max = ymax_widget.value
+        qr2_widget.max = ymax_widget.value
+    xmin_widget.observe(update_xmin, 'value')
+    xmax_widget.observe(update_xmax, 'value')
+    ymin_widget.observe(update_ymin, 'value')
+    ymax_widget.observe(update_ymax, 'value')
+
+    # Organize slider widgets into boxes
+    qleftright = widgets.VBox([widgets.HBox([t_widget, which_char_widget]), 
+                               widgets.HBox([ql1_widget, ql2_widget, rho_widget]),
+                               widgets.HBox([qr1_widget, qr2_widget, bulk_widget])])
+    plot_opts = widgets.VBox([widgets.HBox([show_physical, show_unphysical]),
+                              widgets.HBox([xmin_widget, xmax_widget]),
+                              widgets.HBox([ymin_widget, ymax_widget])])
+
+    # Set up interactive GUI (tab style)
+    interact_gui = widgets.Tab(children=[qleftright, plot_opts])
+    interact_gui.set_title(0, 'Left and right states')
+    interact_gui.set_title(1, 'Plot options')
+
+    # Define interactive widget and run GUI
+    ppwidget = interact(pp_plot, t=t_widget, 
+                        pl=ql1_widget, ul=ql2_widget,
+                        pr=qr1_widget, ur=qr2_widget,
+                        rho=rho_widget, bulk=bulk_widget,
+                        which_char=which_char_widget,
+                        xmin=xmin_widget, xmax=xmax_widget,
+                        ymin=ymin_widget, ymax=ymax_widget,
+                        show_phys=show_physical, show_unphys=show_unphysical)
+    ppwidget.widget.close()
+    display(interact_gui)
+    display(ppwidget.widget.out)
