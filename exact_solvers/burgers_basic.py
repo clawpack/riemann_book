9a5030cb9169e3476dafca0d9a78d0c8d1b7abdd
BB <me@yomama.com>
2022-03-04 13:40:04 +0300

    exact_riemann_solution does not return a function it is the function itself

--- exact_solvers/burgers_basic.py
+++ exact_solvers/burgers_basic.py
@@ -13,67 +13,66 @@ import matplotlib
 def speed(q, xi):
     return q
 
-def exact_riemann_solution(q_l,q_r):
+def exact_riemann_solution(xi,q_l,q_r):
     f = lambda q: 0.5*q*q
-    states = np.array([[q_l, q_r]])
     # Shock wave
     if q_l > q_r: 
         shock_speed = (f(q_l)-f(q_r))/(q_l-q_r)
-        speeds = [[shock_speed,shock_speed]]
-        def reval(xi):
-            q = np.zeros((1,len(xi)))
-            q[0,:] = (xi < shock_speed)*q_l \
-              + (xi >=shock_speed)*q_r
-            return q
+        q = (xi < shock_speed)*q_l \
+          + (xi >=shock_speed)*q_r
+        return q
     # Rarefaction wave
     else:  
         c_l  = q_l
         c_r = q_r
-
-        speeds = [[c_l,c_r]]
-
-        def reval(xi):
-            q = np.zeros((1,len(xi)))
-            q[0,:] = (xi<=c_l)*q_l \
-              + (xi>=c_r)*q_r \
-              + (c_l<xi)*(xi<c_r)*xi
-            return q
-
-    return reval
+        q = (xi<=c_l)*q_l \
+          + (xi>=c_r)*q_r \
+          + (c_l<xi)*(xi<c_r)*xi
+        return q
     
 def rarefaction():
     print ('rarefaction')
     q_l, q_r = 2.0, 4.0
-    reval = exact_riemann_solution(q_l ,q_r)
+    
     for t in np.linspace(0,1,6):
         outfile = '/tmp/rarefaction-%f.png' % t
         print (outfile)
-        plot_riemann(reval, t, outfile)
+
+        fig, ax = plt.subplots(figsize=(5, 3))
+                    
+        x = np.linspace(-4, 4, 1000)
+        
+        q = np.array([exact_riemann_solution(xi/(t+1e-10),q_l,q_r) for xi in x])
+
+        ax.set_xlim(-4,4)
+
+        ax.plot(x,q,'-k',lw=2)
+    
+        plt.savefig(outfile) 
+        
+        #plot_riemann(reval, t, outfile)
 
 def shock():
     print ('shock wave')
     q_l, q_r = 5.0, 1.0
-    reval = exact_riemann_solution(q_l ,q_r)
+
     for t in np.linspace(0,1,6):
         outfile = '/tmp/shock-%f.png' % t
         print (outfile)
-        plot_riemann(reval, t, outfile)
 
-    
-def plot_riemann(riemann_eval, t, outfile):
-    
-    fig, ax = plt.subplots(figsize=(5, 3))
+        fig, ax = plt.subplots(figsize=(5, 3))
                     
-    x = np.linspace(-4, 4, 1000)
+        x = np.linspace(-4, 4, 1000)
+        
+        q = np.array([exact_riemann_solution(xi/(t+1e-10),q_l,q_r) for xi in x])
 
-    q = riemann_eval(x/(t+1e-10))
+        ax.set_xlim(-4,4)
 
-    ax.set_xlim(-4,4)
-
-    ax.plot(x,q[0][:],'-k',lw=2)
+        ax.plot(x,q,'-k',lw=2)
     
-    plt.savefig(outfile) 
+        plt.savefig(outfile) 
 
+        
 if __name__ == "__main__": 
     
     rarefaction()
